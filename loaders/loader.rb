84336e0d574edecce410cf9f3ca263093fcc7c7a
Patricio López <lopezjuripatricio@gmail.com>
2015-10-25 23:30:58 -0300

    better abstract/interfaces

--- loaders/loader.rb
+++ loaders/loader.rb
@@ -1,7 +1,10 @@
+# encoding: UTF-8
+
 # Loads the strategy patterns that fetch fresh news.
 class Loader
   # loads a strategy
   # receives nothing returns a ProviderStrategy
   def providers
+    fail NotImplementedError, 'subclass must implement this method'
   end
 end
--- providers/provider_strategy.rb
+++ providers/provider_strategy.rb
@@ -1,7 +1,10 @@
+# encoding: UTF-8
+
 # Base class for all strategies.
 class ProviderStrategy
   # Recieves the last news (string) fetch from the source
   # and returns a list of new news (list<string>)
   def run(last)
+    fail NotImplementedError, 'subclass must implement this method'
   end
 end
