958adb1d5ca78e9a4e19026adc007c4d544ee187
saoliva <saoliva@uc.cl>
2015-10-29 12:36:30 -0300

    Adding LaCuartaProvider and LaTerceraProvider

--- providers/lib/lacuarta.rb
+++ providers/lib/lacuarta.rb
@@ -1,9 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
+require_relative './../provider_strategy'
 
 # Provider del diario LaCuarta
 class LaCuartaProvider
-  def fetch_news
+  def run(last)
     newsdoc = Nokogiri::HTML(open('http://www.lacuarta.com/feed/manager?type=rss&sc=TEFDVUFSVEE='))
 
     # Getting news
@@ -11,14 +12,16 @@ class LaCuartaProvider
       title = node.xpath('title/text()')
       subtitle = node.xpath('description/text()').text
       url = node.xpath('guid/text()')
-      # No he encontrado los tags.
-      tags = ''
+
+      return unless @persistence_delegate.should_download?(url)
 
       noticedoc = Nokogiri::HTML(open(url.to_s))
       body = fetch_body noticedoc
       image = fetch_image noticedoc
+      tags = fetch_tags noticedoc
 
-      # Aqui iria la union de todas las cosas, aun no se que poner
+      notice = {title: '#{title}', subtitle: '#{subtitle}', url: '#{url}', tags: '#{tags}', body: '#{body}', image: '#{image}' }
+      @persistence_delegate.notify_new(notice)
     end
     news
   end
@@ -36,6 +39,15 @@ class LaCuartaProvider
   # Obtain image in the url
   def fetch_image(image)
     aux = image.xpath("//div[contains(@class, 'L4ta_img_efecto')]/img/@src")
+    aux
+  end
+
+  # Obtain tags in the url
+  def fetch_tags(tags)
+    aux = ''
+    tags.xpath("//ul[contains(@class, 'homeSeccion')]/li/text()").each do |tag|
+      aux << tag.text
+    end
     aux
   end
 end
