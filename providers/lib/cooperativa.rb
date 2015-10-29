7da3a500927a9b41695a40e3a4b27aa25220e532
santiagomarti <marti92@gmail.com>
2015-10-29 20:09:58 -0300

    Added Cooperativa provider, not finished

--- providers/lib/cooperativa.rb
+++ providers/lib/cooperativa.rb
@@ -4,19 +4,18 @@ require 'open-uri'
 require_relative './../provider_strategy'
 
 #El Mostrador Provider
-class ElMostradorProvider < ProviderStrategy
+class CooperativaProvider < ProviderStrategy
 
 	#Inherited method
 	def run(last)
-		doc = Nokogiri::HTML(open('http://www.elmostrador.cl/destacado/feed/'))
+		doc = Nokogiri::HTML(open('http://www.cooperativa.cl/noticias/site/tax/port/all/rss____1.xml'))
 		doc.xpath('//item').collect do |element|
 			url = element.xpath('link')
 			return unless @persistence_delegate.should_download?(url)
 			title = element.xpath('title')
 			image = element.xpath('image')
-			content = element.xpath('content')
 			subtitle = element.xpath('description')
-			notice = {title: '#{title}', subtitle: '#{subtitle}', url: '#{url}', tags: '', body: '#{content}', image: '#{image}' }
+			notice = {title: '#{title}', subtitle: '#{subtitle}', url: '#{url}', tags: '', body: '#{body}', image: '#{image}' }
       		@persistence_delegate.notify_new(notice)
 		end
 	end 	 	
