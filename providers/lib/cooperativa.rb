5024b3d222037829cb89bd85949711fd8c9e59a9
santiagomarti <marti92@gmail.com>
2015-10-30 18:30:40 -0300

    El Mostrador provider ready

--- providers/lib/cooperativa.rb
+++ providers/lib/cooperativa.rb
@@ -1,23 +1,28 @@
-require 'nokogiri'
-require 'open-uri'
+require "nokogiri"
+require "open-uri"
 
-require_relative './../provider_strategy'
+require_relative "./../provider_strategy"
 
 #El Mostrador Provider
-class CooperativaProvider < ProviderStrategy
-
+class CooperativaProvider
 	#Inherited method
 	def run(last)
-		doc = Nokogiri::HTML(open('http://www.cooperativa.cl/noticias/site/tax/port/all/rss____1.xml'))
-		doc.xpath('//item').collect do |element|
-			url = element.xpath('link')
-			return unless @persistence_delegate.should_download?(url)
-			title = element.xpath('title')
-			image = element.xpath('image')
-			subtitle = element.xpath('description')
-			notice = {title: '#{title}', subtitle: '#{subtitle}', url: '#{url}', tags: '', body: '#{body}', image: '#{image}' }
-      		@persistence_delegate.notify_new(notice)
-		end
+		doc = Nokogiri::HTML(open("http://www.cooperativa.cl/noticias/site/tax/port/all/rss____1.xml"))
+		doc.xpath("//item").collect do |element|
+			url = element.xpath("link")
+			#return unless @persistence_delegate.should_download?(url)
+			title = element.xpath("title")
+			image = element.xpath("image")
+			subtitle = ""
+			doc2 = Nokogiri::HTML(open(url.to_s))
+			source = doc2.xpath("//header/div/span/strong").text
+			content = doc2.xpath('//div/p[contains(@class, "CUERPO")]').text
+			notice = {title: "#{title}", source: "#{source}", subtitle: "#{subtitle}", url: "#{url}", tags: "", body: "#{content}", image: "#{image}" }
+	   		puts notice
+	   		#@persistence_delegate.notify_new(notice)
 	end 	 	
   end
 end
+
+c = CooperativaProvider.new
+c.run(nil)
--- providers/lib/elmostrador.rb
+++ providers/lib/elmostrador.rb
@@ -1,24 +1,24 @@
-require 'nokogiri'
-require 'open-uri'
+require "nokogiri"
+require "open-uri"
 
-require_relative './../provider_strategy'
+require_relative "./../provider_strategy"
 
 #El Mostrador Provider
 class ElMostradorProvider < ProviderStrategy
-
 	#Inherited method
 	def run(last)
-		doc = Nokogiri::HTML(open('http://www.elmostrador.cl/destacado/feed/'))
-		doc.xpath('//item').collect do |element|
-			url = element.xpath('link')
+		doc = Nokogiri::HTML(open("http://www.elmostrador.cl/destacado/feed/"))
+		doc.xpath("//item").collect do |element|
+			url = element.xpath("link/following-sibling::text()[1]")
 			return unless @persistence_delegate.should_download?(url)
-			title = element.xpath('title')
-			image = element.xpath('image')
-			content = element.xpath('content')
-			subtitle = element.xpath('description')
-			notice = {title: '#{title}', subtitle: '#{subtitle}', url: '#{url}', tags: '', body: '#{content}', image: '#{image}' }
-      		@persistence_delegate.notify_new(notice)
+			title = element.xpath("title")
+			image = element.xpath("image")
+			subtitle = ""
+			doc2 = Nokogiri::HTML(open(url.to_s))
+			source = doc2.xpath('//p[contains(@class, "autor-y-fecha")]/span/a').text
+			content = doc2.xpath('//div[contains(@class, "cuerpo-noticia")]').text
+			notice = {title: "#{title}", source: "#{source}", subtitle: "#{subtitle}", url: "#{url}", tags: "", body: "#{content}", image: "#{image}" }
+	   		@persistence_delegate.notify_new(notice)
 		end
 	end 	 	
-  end
 end
