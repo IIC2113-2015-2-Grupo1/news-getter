76fa91ea01bbaab9c8ef95027f253a82f0670ea6
santiagomarti <marti92@gmail.com>
2015-10-30 20:03:39 -0300

    Cooperativa provider ready, and Rubocop

--- providers/lib/cooperativa.rb
+++ providers/lib/cooperativa.rb
@@ -1,28 +1,24 @@
-require "nokogiri"
-require "open-uri"
+require 'nokogiri'
+require 'open-uri'
 
-require_relative "./../provider_strategy"
+require_relative './../provider_strategy'
 
-#El Mostrador Provider
-class CooperativaProvider
-	#Inherited method
-	def run(last)
-		doc = Nokogiri::HTML(open("http://www.cooperativa.cl/noticias/site/tax/port/all/rss____1.xml"))
-		doc.xpath("//item").collect do |element|
-			url = element.xpath("link")
-			#return unless @persistence_delegate.should_download?(url)
-			title = element.xpath("title")
-			image = element.xpath("image")
-			subtitle = ""
-			doc2 = Nokogiri::HTML(open(url.to_s))
-			source = doc2.xpath("//header/div/span/strong").text
-			content = doc2.xpath('//div/p[contains(@class, "CUERPO")]').text
-			notice = {title: "#{title}", source: "#{source}", subtitle: "#{subtitle}", url: "#{url}", tags: "", body: "#{content}", image: "#{image}" }
-	   		puts notice
-	   		#@persistence_delegate.notify_new(notice)
-	end 	 	
+# Cooperativa Provider
+class CooperativaProvider < ProviderStrategy
+  # Inherited method
+  def run(last)
+    doc = Nokogiri::HTML(open('http://www.cooperativa.cl/noticias/site/tax/port/all/rss____1.xml'))
+    doc.xpath('//item').collect do |element|
+      url = element.xpath('link/following-sibling::text()[1]').text.strip
+      return unless @persistence_delegate.should_download?(url)
+      title = element.xpath('title').text.strip
+      image = 'http://www.cooperativa.cl' + element.xpath('prontus_fotolibre').text.strip
+      subtitle = ''
+      doc2 = Nokogiri::HTML(open(url.to_s))
+      source = doc2.xpath('//header/div/span/strong').text.strip
+      content = doc2.xpath("//div[contains(@class, 'CUERPO')]/p").text
+      notice = {title: "#{title}", source: "#{source}", subtitle: "#{subtitle}", url: "#{url}", tags: "", body: "#{content}", image: "#{image}" }
+      @persistence_delegate.notify_new(notice)
+    end
   end
 end
-
-c = CooperativaProvider.new
-c.run(nil)
